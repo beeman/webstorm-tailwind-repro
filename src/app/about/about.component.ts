22741ce1de15d06585b7f4b1e562317c31acbc0e
Bram Borggreve <borggreve@gmail.com>
2020-12-30 17:20:17 -0500

    feat: add some tailwind styles

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <div class="text-pink-700 bg-purple-800 h-full">
+      About Page
+    </div>
+  `,
+})
+export class AboutComponent {}
